1568cac4bb67a031bdea94d978c6e8dac7b7c728
Gary Dong <garydong@crc-dot1x-nat-10-239-1-17.bu.edu>
2024-02-12 16:12:05 -0500

    implemented remaining functionality

--- app/src/main/java/com/example/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.kt
@@ -8,6 +8,8 @@ import com.example.temperatureconverter.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var f_val = 0
+    private var c_val = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +20,9 @@ class MainActivity : AppCompatActivity() {
         binding.CelsiusSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 binding.CelsiusProgress.text = "$progress°C"
+                f_val = (progress * 9/5) + 32
+                binding.FahrenheitSeekBar.setProgress(f_val, true)
+                updateMessage()
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
             }
@@ -30,6 +35,16 @@ class MainActivity : AppCompatActivity() {
         binding.FahrenheitSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 binding.FahrenheitProgress.text = "$progress°F"
+                c_val = (progress - 32) * 5/9
+                if (c_val >= 0){
+                    binding.CelsiusSeekBar.setProgress(c_val, true)
+                }
+                else {
+                    binding.FahrenheitSeekBar.setProgress(32, true)
+                }
+                updateMessage()
+
+
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
             }
@@ -37,5 +52,13 @@ class MainActivity : AppCompatActivity() {
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
             }
         })
+    }
+    private fun updateMessage(){
+        if ( c_val <= 20 ) {
+            binding.interestingMessage.setText(R.string.interesting_message_warmer)
+        }
+        else {
+            binding.interestingMessage.setText(R.string.interesting_message_cooler)
+        }
     }
 }
